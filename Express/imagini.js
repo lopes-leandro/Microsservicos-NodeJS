d45d03849a5207d57344b844c375512e3c853e66
Leandro Lopes <leandro_lopez12@yahoo.com.br>
2019-08-20 03:22:26 -0300

    criado o serviço /uploads que irá receber a imagem postada para o nosso serviço

--- Express/imagini.js
+++ Express/imagini.js
@@ -1,6 +1,32 @@
+const bodyparser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const sharp = require("sharp");
 const app = express();
+
+app.post("/uploads/:image", bodyparser.raw({
+    limit: "100mb",
+    type: "image/*"
+}), (req, res) => {
+    let image = req.params.image.toLowerCase();
+
+    if (!image.match(/\.(pgn|jpg)$/)) {
+        return res.status(403).end();
+    }
+
+    let len = req.body.length;
+    let fd = fs.createWriteStream(path.join(__dirname, "uploads", image),{
+        flags: "w+",
+        encoding: "binary"
+    });
+
+    fd.write(req.body);
+    fd.end();
+    fd.on("close", () => {
+        res.send({status: "ok", size: len});
+    });
+});
 
 app.get(/\/thumbnail\.(jpg|png|tiff)/, (req, res, next) => {
     let format = (req.params[0] == "png" ? "png" : "jpg");
@@ -48,7 +74,7 @@ app.get(/\/thumbnail\.(jpg|png|tiff)/, (req, res, next) => {
             fill="${textcolor}" text-anchor="middle">${width} x ${height}</text>
     </svg>`
     );
-    image.composite([{ input: thumbnail }]).toFormat(format).pipe(res);    
+    image.composite([{ input: thumbnail }]).toFormat(format).pipe(res);
 });
 
 app.listen(3000, () => {
