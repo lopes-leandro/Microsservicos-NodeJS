d0e59241011c74bc29f99ffe65f9740be1b291ef
Leandro Lopes <leandro_lopez12@yahoo.com.br>
2019-08-25 03:04:12 -0300

    criação do endpoint getImage

--- Express/imagini.js
+++ Express/imagini.js
@@ -5,6 +5,28 @@ const express = require("express");
 const sharp = require("sharp");
 const app = express();
 
+app.get("/uploads/:image", (req, res) => {
+    let ext = path.extname(req.params.image);
+
+    if (!ext.match(/^\.(png|jpg)$/)) {
+        return res.status(404).end();
+    }
+
+    let fd = fs.createReadStream(path.join(__dirname, "uploads", req.params.image));
+
+    fd.on("error", (e) => {
+        if (e.code == "ENOENT") {
+            return res.status(404).end();
+        }
+
+        res.status(500).end();
+    });
+
+    res.setHeader("Content-Type", "image/" + ext.substr(1));
+
+    fd.pipe(res);
+});
+
 app.head("/uploads/:image", (req, res) => {
     fs.access(
         path.join(__dirname, "uploads", req.params.image),
@@ -40,54 +62,54 @@ app.post("/uploads/:image", bodyparser.raw({
     });
 });
 
-app.get(/\/thumbnail\.(jpg|png|tiff)/, (req, res, next) => {
-    let format = (req.params[0] == "png" ? "png" : "jpg");
-    let width = +req.query.width || 300;
-    let height = +req.query.height || 200;
-    let border = 5;
-    let bgcolor = "#fcfcfc";
-    let fgcolor = req.query.fgcolor || "#ddd";
-    let textcolor = "#000";
-    let textsize = 24;
-    let image = sharp({
-        create: {
-            width: width,
-            height: height,
-            channels: 4,
-            background: { r: 0, g: 0, b: 0, alpha: 0.5 }
-        }
-    });
+// app.get(/\/thumbnail\.(jpg|png|tiff)/, (req, res, next) => {
+//     let format = (req.params[0] == "png" ? "png" : "jpg");
+//     let width = +req.query.width || 300;
+//     let height = +req.query.height || 200;
+//     let border = 5;
+//     let bgcolor = "#fcfcfc";
+//     let fgcolor = req.query.fgcolor || "#ddd";
+//     let textcolor = "#000";
+//     let textsize = 24;
+//     let image = sharp({
+//         create: {
+//             width: width,
+//             height: height,
+//             channels: 4,
+//             background: { r: 0, g: 0, b: 0, alpha: 0.5 }
+//         }
+//     });
 
-    const thumbnail = new Buffer.from(
-        `<svg width="${width}" height="${height}">
-        <rect
-            x="0" y="0"
-            width="${width}" height="${height}"
-            fill="${fgcolor}" />
-        <rect
-            x="${border}" y="${border}"
-            width="${width - border * 2}" height="${height - border * 2}"
-            fill="${bgcolor}" />
-        <line
-            x1="${border * 2}" y1="${border * 2}"
-            x2="${width - border * 2}" y2="${height - border * 2}"
-            stroke-width="${border}" stroke="${fgcolor}" />
-        <line
-            x1="${width - border * 2}" y1="${border * 2}"
-            x2="${border * 2}" y2="${height - border * 2}"
-            stroke-width="${border}" stroke="${fgcolor}" />
-        <rect
-            x="${border}" y="${(height - textsize) / 2}"
-            width="${width - border * 2}" height="${textsize}"
-            fill="${bgcolor}" />
-        <text
-            x="${width / 2}" y="${height / 2}" dy="8"
-            font-family="Sans" font-size="${textsize}"
-            fill="${textcolor}" text-anchor="middle">${width} x ${height}</text>
-    </svg>`
-    );
-    image.composite([{ input: thumbnail }]).toFormat(format).pipe(res);
-});
+//     const thumbnail = new Buffer.from(
+//         `<svg width="${width}" height="${height}">
+//         <rect
+//             x="0" y="0"
+//             width="${width}" height="${height}"
+//             fill="${fgcolor}" />
+//         <rect
+//             x="${border}" y="${border}"
+//             width="${width - border * 2}" height="${height - border * 2}"
+//             fill="${bgcolor}" />
+//         <line
+//             x1="${border * 2}" y1="${border * 2}"
+//             x2="${width - border * 2}" y2="${height - border * 2}"
+//             stroke-width="${border}" stroke="${fgcolor}" />
+//         <line
+//             x1="${width - border * 2}" y1="${border * 2}"
+//             x2="${border * 2}" y2="${height - border * 2}"
+//             stroke-width="${border}" stroke="${fgcolor}" />
+//         <rect
+//             x="${border}" y="${(height - textsize) / 2}"
+//             width="${width - border * 2}" height="${textsize}"
+//             fill="${bgcolor}" />
+//         <text
+//             x="${width / 2}" y="${height / 2}" dy="8"
+//             font-family="Sans" font-size="${textsize}"
+//             fill="${textcolor}" text-anchor="middle">${width} x ${height}</text>
+//     </svg>`
+//     );
+//     image.composite([{ input: thumbnail }]).toFormat(format).pipe(res);
+// });
 
 app.listen(3000, () => {
     console.log("ready");
