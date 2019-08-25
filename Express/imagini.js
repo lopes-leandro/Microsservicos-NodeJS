f7f32f18b2fd93670cd33ed93f67ad02eac3cf93
Leandro Lopes <leandro_lopez12@yahoo.com.br>
2019-08-25 18:00:09 -0300

    criado o pre-processador dos parâmetros de rota para validar as imagens

--- Express/imagini.js
+++ Express/imagini.js
@@ -5,6 +5,18 @@ const express = require("express");
 const sharp = require("sharp");
 const app = express();
 
+app.param("image", (req, res, next, image) => {
+    if (!image.match(/\.(png|jpg)$/i)) {
+        return res.status(req.method == "POST" ? 403 : 404).end();
+    }
+
+    req.image = image;
+
+    req.localpath = path.join(__dirname, "uploads", req.image);
+
+    return next();
+});
+
 app.get("/uploads/:image", (req, res) => {
     let ext = path.extname(req.params.image);
 
@@ -16,9 +28,13 @@ app.get("/uploads/:image", (req, res) => {
 
     fd.on("error", (e) => {
         if (e.code == "ENOENT") {
-            return res.status(404).end();
+            res.status(404).end();
+            if (req.accepts('html')) {
+                res.setHeader("Content-Type", "text/html");
+                res.write("<strong>Error:</strong> image not found");
+            }
+            return res.end();
         }
-
         res.status(500).end();
     });
 
@@ -43,73 +59,67 @@ app.post("/uploads/:image", bodyparser.raw({
     limit: "100mb",
     type: "image/*"
 }), (req, res) => {
-    let image = req.params.image.toLowerCase();
-
-    if (!image.match(/\.(png|jpg)$/)) {
-        return res.status(403).end();
-    }
-
-    let len = req.body.length;
-    let fd = fs.createWriteStream(path.join(__dirname, "uploads", image),{
+    
+    let fd = fs.createWriteStream(req.localpath, {
         flags: "w+",
         encoding: "binary"
     });
 
-    fd.write(req.body);
-    fd.end();
+    fd.end(req.body);
+
     fd.on("close", () => {
-        res.send({status: "ok", size: len});
+        res.send({status: "ok", size: req.body.length});
     });
 });
 
-// app.get(/\/thumbnail\.(jpg|png|tiff)/, (req, res, next) => {
-//     let format = (req.params[0] == "png" ? "png" : "jpg");
-//     let width = +req.query.width || 300;
-//     let height = +req.query.height || 200;
-//     let border = 5;
-//     let bgcolor = "#fcfcfc";
-//     let fgcolor = req.query.fgcolor || "#ddd";
-//     let textcolor = "#000";
-//     let textsize = 24;
-//     let image = sharp({
-//         create: {
-//             width: width,
-//             height: height,
-//             channels: 4,
-//             background: { r: 0, g: 0, b: 0, alpha: 0.5 }
-//         }
-//     });
+app.get(/\/thumbnail\.(jpg|png|tiff)/, (req, res, next) => {
+    let format = (req.params[0] == "png" ? "png" : "jpg");
+    let width = +req.query.width || 300;
+    let height = +req.query.height || 200;
+    let border = 5;
+    let bgcolor = "#fcfcfc";
+    let fgcolor = req.query.fgcolor || "#ddd";
+    let textcolor = "#000";
+    let textsize = 24;
+    let image = sharp({
+        create: {
+            width: width,
+            height: height,
+            channels: 4,
+            background: { r: 0, g: 0, b: 0, alpha: 0.5 }
+        }
+    });
 
-//     const thumbnail = new Buffer.from(
-//         `<svg width="${width}" height="${height}">
-//         <rect
-//             x="0" y="0"
-//             width="${width}" height="${height}"
-//             fill="${fgcolor}" />
-//         <rect
-//             x="${border}" y="${border}"
-//             width="${width - border * 2}" height="${height - border * 2}"
-//             fill="${bgcolor}" />
-//         <line
-//             x1="${border * 2}" y1="${border * 2}"
-//             x2="${width - border * 2}" y2="${height - border * 2}"
-//             stroke-width="${border}" stroke="${fgcolor}" />
-//         <line
-//             x1="${width - border * 2}" y1="${border * 2}"
-//             x2="${border * 2}" y2="${height - border * 2}"
-//             stroke-width="${border}" stroke="${fgcolor}" />
-//         <rect
-//             x="${border}" y="${(height - textsize) / 2}"
-//             width="${width - border * 2}" height="${textsize}"
-//             fill="${bgcolor}" />
-//         <text
-//             x="${width / 2}" y="${height / 2}" dy="8"
-//             font-family="Sans" font-size="${textsize}"
-//             fill="${textcolor}" text-anchor="middle">${width} x ${height}</text>
-//     </svg>`
-//     );
-//     image.composite([{ input: thumbnail }]).toFormat(format).pipe(res);
-// });
+    const thumbnail = new Buffer.from(
+        `<svg width="${width}" height="${height}">
+        <rect
+            x="0" y="0"
+            width="${width}" height="${height}"
+            fill="${fgcolor}" />
+        <rect
+            x="${border}" y="${border}"
+            width="${width - border * 2}" height="${height - border * 2}"
+            fill="${bgcolor}" />
+        <line
+            x1="${border * 2}" y1="${border * 2}"
+            x2="${width - border * 2}" y2="${height - border * 2}"
+            stroke-width="${border}" stroke="${fgcolor}" />
+        <line
+            x1="${width - border * 2}" y1="${border * 2}"
+            x2="${border * 2}" y2="${height - border * 2}"
+            stroke-width="${border}" stroke="${fgcolor}" />
+        <rect
+            x="${border}" y="${(height - textsize) / 2}"
+            width="${width - border * 2}" height="${textsize}"
+            fill="${bgcolor}" />
+        <text
+            x="${width / 2}" y="${height / 2}" dy="8"
+            font-family="Sans" font-size="${textsize}"
+            fill="${textcolor}" text-anchor="middle">${width} x ${height}</text>
+    </svg>`
+    );
+    image.composite([{ input: thumbnail }]).toFormat(format).pipe(res);
+});
 
 app.listen(3000, () => {
     console.log("ready");
