c3dc0582584e996dbbf13f15fa8af18c91132276
Leandro Lopes <leandro_lopez12@yahoo.com.br>
2019-08-26 02:36:10 -0300

    removemos todos os parâmetros de rota (width, height e greyscale), mantemos o (image), removemos todas as rotas de downloads, mantendo a rota mais simples /uploads/:image

--- Express/imagini.js
+++ Express/imagini.js
@@ -5,17 +5,6 @@ const express = require("express");
 const sharp = require("sharp");
 const app = express();
 
-//  altera nossa rota de download
-app.param("width", (req, res, next, width) => {
-    req.width = +width;
-    return next();
-});
-
-app.param("height", (req, res, next, height) => {
-    req.height = +height;
-    return next();
-});
-
 app.param("image", (req, res, next, image) => {
     if (!image.match(/\.(png|jpg)$/i)) {
         return res.status(req.method == "POST" ? 403 : 404).end();
@@ -24,16 +13,6 @@ app.param("image", (req, res, next, image) => {
     req.image = image;
 
     req.localpath = path.join(__dirname, "uploads", req.image);
-
-    return next();
-});
-
-// indica ao serviço para retornar a imagem em tons de cinza
-app.param("greyscale", (req, res, next, greyscale) => {
-    
-    if (greyscale != "bw") return next("route");
-
-    req.greyscale = true;
 
     return next();
 });
@@ -65,30 +44,7 @@ function download_image(req, res) {
     });
 }
 
-app.get("/uploads/:width(\\d+)x:height(\\d+)-:greyscale-:image", download_image);
-app.get("/uploads/:width(\\d+)x:height(\\d+)-:image", download_image);
-
-app.get("/uploads/_x:height(\\d+)-:greyscale-:image", download_image);
-app.get("/uploads/_x:height(\\d+)-:image", download_image);
-
-app.get("/uploads/:width(\\d+)x_-:greyscale-:image", download_image);
-app.get("/uploads/:width(\\d+)x_-:image", download_image);
-
-app.get("/uploads/:greyscale-:image", download_image);
 app.get("/uploads/:image", download_image);
-
-// app.get("/uploads/:image", (req, res) => {
-
-//     let fd = fs.createReadStream(req.localpath);
-
-//     fd.on("error", (e) => {
-//         res.status(e.code == "ENOENT" ? 404 : 500).end();
-//     });
-
-//     res.setHeader("Content-Type", "image/" + path.extname(req.image).substr(1));
-
-//     fd.pipe(res);
-// });
 
 app.head("/uploads/:image", (req, res) => {
     fs.access(req.localpath, fs.constants.R_OK, (err) => {
@@ -96,7 +52,6 @@ app.head("/uploads/:image", (req, res) => {
     }
     );
 });
-
 
 app.post("/uploads/:image", bodyparser.raw({limit: "100mb", type: "image/*"}), (req, res) => {
 
@@ -110,55 +65,6 @@ app.post("/uploads/:image", bodyparser.raw({limit: "100mb", type: "image/*"}), (
     fd.on("close", () => {
         res.send({ status: "ok", size: req.body.length });
     });
-});
-
-app.get(/\/thumbnail\.(jpg|png|tiff)/, (req, res, next) => {
-    let format = (req.params[0] == "png" ? "png" : "jpg");
-    let width = +req.query.width || 300;
-    let height = +req.query.height || 200;
-    let border = 5;
-    let bgcolor = "#fcfcfc";
-    let fgcolor = req.query.fgcolor || "#ddd";
-    let textcolor = "#000";
-    let textsize = 24;
-    let image = sharp({
-        create: {
-            width: width,
-            height: height,
-            channels: 4,
-            background: { r: 0, g: 0, b: 0, alpha: 0.5 }
-        }
-    });
-
-    const thumbnail = new Buffer.from(
-        `<svg width="${width}" height="${height}">
-        <rect
-            x="0" y="0"
-            width="${width}" height="${height}"
-            fill="${fgcolor}" />
-        <rect
-            x="${border}" y="${border}"
-            width="${width - border * 2}" height="${height - border * 2}"
-            fill="${bgcolor}" />
-        <line
-            x1="${border * 2}" y1="${border * 2}"
-            x2="${width - border * 2}" y2="${height - border * 2}"
-            stroke-width="${border}" stroke="${fgcolor}" />
-        <line
-            x1="${width - border * 2}" y1="${border * 2}"
-            x2="${border * 2}" y2="${height - border * 2}"
-            stroke-width="${border}" stroke="${fgcolor}" />
-        <rect
-            x="${border}" y="${(height - textsize) / 2}"
-            width="${width - border * 2}" height="${textsize}"
-            fill="${bgcolor}" />
-        <text
-            x="${width / 2}" y="${height / 2}" dy="8"
-            font-family="Sans" font-size="${textsize}"
-            fill="${textcolor}" text-anchor="middle">${width} x ${height}</text>
-    </svg>`
-    );
-    image.composite([{ input: thumbnail }]).toFormat(format).pipe(res);
 });
 
 app.listen(3000, () => {
